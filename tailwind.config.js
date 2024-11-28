67a4e0bcadeb77b54a8376532adfb48d67f00fb7
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2024-11-28 17:01:05 +0000

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'euruko-primary': '#FF0000', // You can adjust this color
+      },
+    },
+  },
+  plugins: [],
+}
